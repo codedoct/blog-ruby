5fc50669614191f3e7727321d2567cca9d48d6f3
victra <victra.sonata@gmail.com>
2016-11-26 13:52:30 +0700

    finish crud

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,28 @@ class UsersController < ApplicationController
 	  end 
 	end
 
+	def _show
+		@user = User.find(params[:id])
+	end
+
+	def _edit
+		@user = User.find(params[:id])
+		if @user.update(user_params)
+			redirect_to '/users' 
+		else 
+			redirect_to '/' 
+		end 
+	end
+
+	def _delete
+		@user = User.find(params[:id])
+		if @user.destroy
+			redirect_to '/users' 
+		else 
+			redirect_to '/' 
+		end 
+	end
+
 	private
 		def user_params
 			params.require(:user).permit(:first_name, :last_name, :nick_name, :address, :phone)
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # root 'application#hello'
   get 'users' => 'users#index'
+
   get '' => 'home#index'
+
   get 'signup' => 'users#_signup'
   post 'signup' => 'users#_addUser'
+
+  get 'edit/:id' => 'users#_show', as: :user
+  post 'edit/:id' => 'users#_edit'
+
+  delete 'delete/:id' => 'users#_delete'
 end
