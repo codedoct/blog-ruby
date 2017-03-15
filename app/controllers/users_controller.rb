dde167f6db07ae06b13648f54e578d6f750c242e
victra <victra.sonata@gmail.com>
2017-03-15 11:20:50 +0700

    kantor baru

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,4 +42,6 @@ class UsersController < ApplicationController
 		def user_params
 			params.require(:user).permit(:first_name, :last_name, :nick_name, :address, :phone, :email, :password)
 		end
+
+# kantor baru
 end
